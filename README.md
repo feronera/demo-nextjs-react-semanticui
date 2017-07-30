2ad2ccba47a1b8f86e271080d4b5aede0192347d
NATTHAWAT BOONCHAISEREE <feronera@gmail.com>
2017-07-30 15:55:38 +0700

    Add example withRedux page from https://github.com/zeit/next.js/blob/master/examples/with-redux

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # demo-nextjs-react-semanticui
+
+
+## With Redux
+
+https://github.com/zeit/next.js/blob/master/examples/with-redux
+
+```
+yarn add redux react-redux redux-thunk next-redux-wrapper
+```
---
+++ pages/example/with-redux.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { initStore, startClock, addCount, serverRenderClock } from '../../store/example-store'
+import withRedux from 'next-redux-wrapper'
+import Page from '../../components/clock/Page'
+
+class WithRedux extends React.Component {
+  static getInitialProps ({ store, isServer }) {
+    store.dispatch(serverRenderClock(isServer))
+    store.dispatch(addCount())
+
+    return { isServer }
+  }
+
+  componentDidMount () {
+    this.timer = this.props.startClock()
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <Page title='Index Page' linkTo='/other' />
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCount: bindActionCreators(addCount, dispatch),
+    startClock: bindActionCreators(startClock, dispatch)
+  }
+}
+
+export default withRedux(initStore, null, mapDispatchToProps)(WithRedux)
