f34069eb98a4ea91ebb3befc22c542b71b6e7819
NATTHAWAT BOONCHAISEREE <feronera@gmail.com>
2017-07-30 15:40:32 +0700

    Add example for using ExampleComponent.

---
+++ pages/example/with-component.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import React from "react";
+import ExampleButton from "../../components/example/ExampleButton"
+
+import mockData from "../../mocks/example/data.json";
+
+const WithComponent = props =>
+  <div>
+    <ExampleButton></ExampleButton>
+  </div>;
+
+export default WithComponent;
